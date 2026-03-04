0b59ba43a0ee29974180aa7e27bd82b64101df7f
specly-devices <speclyspec@gmail.com>
2026-03-04 18:24:36 +0530

    Minimal test app

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("StockAI Test")
+st.write("If you see this, deployment works!")
+```
+
+Update your `requirements.txt` to just:
+```
+streamlit==1.40.0
+altair==5.3.0
